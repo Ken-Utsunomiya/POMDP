8ae6e77aff09e7e4da10d0fb91e1d8a245ba224e
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-10-01 13:50:13 -0700

    refactor: move grid class to index.js for temporary use

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
-const {Grid, directions} = require("./grid");
+
+const directions = {
+    UP: "up",
+    DOWN: "down",
+    LEFT: "left",
+    RIGHT: "right"
+}
+
+class Grid {
+    #belief_states
+
+    constructor(bs) {
+        this.#belief_states = bs
+    }
+
+    get belief_states() {
+        return this.#belief_states
+    }
+
+    update_belief_states(a, e) {
+        let new_bs = [
+            [0,0,0,0],
+            [0,null,0,0],
+            [0,0,0,0]
+        ]
+        console.log(new_bs)
+        console.log(a)
+        console.log(e)
+    }
+
+    output_belief_states() {
+        console.log(this.belief_states)
+    }
+
+    is_terminal(coord) {
+
+    }
+
+    is_third_col(coord) {
+
+    }
+}
+
+// Run the program
+// -------------------------------------------------------
 
 const start = (actions, evidence, belief_states) => {
     const grid = new Grid(belief_states)
